63623c19cc136c78d3504b41e50158797840753c
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-05-18 17:22:21 -0400

    Add interviewerList components and styles of it

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,33 @@
-import React from "react"
-import "components/InterviewerListItem.scss"
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+
 export default function InterviewerListItem(props) {
-  const {id,name,avatar,setInterviewer} =props
+  const {id,name,avatar,setInterviewer,selected} = props;
+  
+  const interviewerClass = classNames("interviewers__item",{
+    "interviewers__item--selected": selected,
+  })
+  const imgClass = classNames({
+    "interviewers__item-image": avatar,
+    "interviewers__item-image--selected-image":avatar && selected
+  })
+  
+  function formatName(name,selected){
+    if (selected) {
+      return `${name}`
+    }
+  }
+
   return (
-    <li className="interviewers__item" onClick={()=>{setInterviewer(id)}}>
+    <li className={interviewerClass} onClick={()=>{setInterviewer(id)}}>
       <img
-        className="interviewers__item-image"
+        className={imgClass}
         src={avatar}
         alt={name}
       />
-      {name}
+      {formatName(name,selected)}
     </li>
   )
 
