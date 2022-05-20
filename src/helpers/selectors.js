052a93c4beda6db4d34e43f2976bfc4b152b9224
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-05-20 00:55:09 -0400

    Add helper function and Change states into objects

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,47 @@
+
 export function getAppointmentsForDay(state, day) {
 
-  const result = [];
+  const findDay = state.days.find(e =>
+    e.name === day
+  )
+  const appointments = state.appointments
+  if(!findDay) {
+    return []
+  }
+  const result = findDay.appointments.map(e => {
+    return appointments[e]
+  })
+  
+  return result
+
+  
 
 
-  //... returns an array of appointments for that day
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-
-
-// function selectUserByName(state, name) {
-//   const filteredNames = state.users.filter(user => user.name === name);
-//   return filteredNames;
-// }
